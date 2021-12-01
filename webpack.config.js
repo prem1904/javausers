71b1396460499ee69ae8f727e72a2f12932d3d7f
unknown <tprem3006@gmail.com>
2021-12-01 08:17:22 +0530

    server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
+    mode: 'production',
     devServer: {
         proxy: {
             '/userManagements/*': {
